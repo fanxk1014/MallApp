2085b87703cb57ea8e195a67e646656edf0d5721
xiaokun <fanxk1014@foxmail.com>
2018-12-30 22:03:35 +0800

    swipe+axios+mockTest

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -13,6 +13,15 @@
                 </van-col>
             </van-row>
         </div>
+
+        <div class="swipe-area">
+            <van-swipe :autoplay="3000">
+                <van-swipe-item v-for="(item, index) in bannerArr" :key="index">
+                    <img v-lazy="item.bannerUrl" width="100%"/>
+                </van-swipe-item>
+            </van-swipe>
+        </div>
+
     </div>
 </template>
 
@@ -20,7 +29,12 @@
 export default {
     data() {
         return {
-            locationIcon: require('../../assets/icon/location.png')
+            locationIcon: require('../../assets/icon/location.png'),
+            bannerArr: [
+                {bannerUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546186074581&di=2c45e29d5389a3f136a0e6c011ada1dc&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01c350598440cc000000212954941d.jpg%401280w_1l_2o_100sh.png'},
+                {bannerUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546185939244&di=b64bf39c0f1851577af7f7c9500dddc8&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0183145a0165e4a801216a4b97a488.jpg%402o.jpg'},
+                {bannerUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546186005716&di=3d52b0e0a93861fac736dd26af502dcd&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0154345a0165eca801202b0cfd379e.jpg%402o.jpg'}
+            ]
         }
     },
 }
@@ -31,6 +45,7 @@ export default {
         height: 2.2rem;
         line-height: 2.2rem;
         background: #4291ab;
+        overflow: hidden;
     }
     .location-icon{
         width: 50%;
@@ -46,5 +61,10 @@ export default {
     }
     .search-button{
         margin-left: 1rem;
+    }
+    .swipe-area{
+        clear: both;
+        max-height: 15rem;
+        overflow: hidden;
     }
 </style>
