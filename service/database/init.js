fe83cfb467f9a4a8481d2ffefbdda03fbd0ee105
xiaokun <fanxk1014@foxmail.com>
2019-01-08 18:50:16 +0800

    数据库增加异步promise

--- service/database/init.js
+++ service/database/init.js
@@ -5,22 +5,43 @@ exports.connect=()=>{
     //连接数据库
     mongoose.connect(db);
 
-    mongoose.connection.on('disconnected',()=>{
-        //进行重连
-        console.log('*******************数据库错误');
-        mongoose.connect(db);
+    let maxConnectTimes = 0;
+
+    return new Promise((resolve,reject)=>{
+        
+        mongoose.connection.on('disconnected',()=>{
+            console.log('************数据库错误************');
+            //进行重连
+            if(maxConnectTimes<=3){
+                maxConnectTimes++;
+                mongoose.connect(db);
+            }else{
+                reject();
+                throw new Error('数据库错误，无法连接，请管理员处理')
+            }
+            
+        })
+
+        //数据库报错
+        mongoose.connection.on('error',err=>{
+            //进行重连
+            if(maxConnectTimes<=3){
+                maxConnectTimes++;
+                mongoose.connect(db);
+            }else{
+                reject(err);
+                console.log('************数据库错误************');
+            }
+        
+        })
+
+        //连接成功打开
+        mongoose.connection.once('open',()=>{
+            console.log('mongodb connected successfully.');
+            resolve();
+        })
     })
 
-    //数据库报错
-    mongoose.connection.on('error',err=>{
-        console.log('*******************数据库错误');
-        console.log(err);
-        mongoose.connect(db);
-    })
-
-    //连接成功打开
-    mongoose.connection.once('open',()=>{
-        console.log('mongodb connected successfully.')
-    })
+    
 
 }
