29d549029d814942bac7093031b59b50d549afbb
xiaokun <fanxk1014@foxmail.com>
2019-01-11 00:20:42 +0800

    koa-router模块化

--- service/index.js
+++ service/index.js
@@ -2,29 +2,40 @@ const Koa = require('koa')
 const app = new Koa()
 const mongoose = require('mongoose')
 const { connect, initSchemas } = require('./database/init.js')
+const Router = require('koa-router')
 
-;(async ()=>{
-    await connect();
-    initSchemas();
+let user = require('./controller/user.js')
 
-    const User = mongoose.model('User');
-    let oneUser = new User({
-        userName:'xiaokun',
-        password:'123456'
-    })
+//装在所有子路由
+let router = new Router();
+router.use('/user',user.routes())
 
-    //插入数据
-    oneUser.save().then(()=>{
-        console.log('---------------数据插入成功---------------');
-    })
+//加载路由中间件
+app.use(router.routes())
+app.use(router.allowedMethods())
 
-    //查询
-    // let  users = await  User.findOne({}).exec()
-    // console.log('------------------')
-    // console.log(users)
-    // console.log('------------------')
+// ;(async ()=>{
+//     await connect();
+//     initSchemas();
 
-})()
+//     const User = mongoose.model('User');
+//     let oneUser = new User({
+//         userName:'xiaokun',
+//         password:'123456'
+//     })
+
+//     //插入数据
+//     oneUser.save().then(()=>{
+//         console.log('---------------数据插入成功---------------');
+//     })
+
+//     //查询
+//     // let  users = await  User.findOne({}).exec()
+//     // console.log('------------------')
+//     // console.log(users)
+//     // console.log('------------------')
+
+// })()
 
 app.use(async(ctx)=>{
     ctx.body='<h3>koa2.</h3>'
