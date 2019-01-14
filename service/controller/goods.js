b00d645eadaf4c21735b40eb274dd5afbcdcb257
xiaokun <fanxk1014@foxmail.com>
2019-01-15 00:40:37 +0800

    完成商品大类接口，并上传数据

--- service/controller/goods.js
+++ service/controller/goods.js
@@ -1,51 +1,48 @@
-// const mongoose = require('mongoose')
-// const fs = require('fs')
-// const Router = require('koa-router')
-// let router = new Router()
-
-// router.get('/insertAllGoodsInfo',async(ctx)=>{
-
-//     fs.readFile('../data_json/newGoods.json','utf8',(err,data)=>{
-//         data = JSON.parse(data)
-//         let saveCount = 0;
-//         const Goods = mongoose.model('Goods');
-//         data.map((value,index)=>{
-//             console.log(value);
-//             let newGoods = Goods(value);
-//             newGoods.save().then(()=>{
-//                 saveCount++;
-//                 console.log('成功'+saveCount);
-//             }).catch(error=>{
-//                 console.log('失败'+saveCount);
-//             })
-//         });
-//         ctx.body='开始导入数据';
-//     })
-
-// })
-
-// module.exports=router
-
-const Router = require ('koa-router')
-let router = new Router()
 const mongoose = require('mongoose')
 const fs = require('fs')
+const Router = require('koa-router')
+let router = new Router()
+
 router.get('/insertAllGoodsInfo',async(ctx)=>{
-     fs.readFile('./data_json/newGoods.json','utf8',(err,data)=>{
-        data=JSON.parse(data)
-        let saveCount=0
-        const Goods = mongoose.model('Goods')
+
+    fs.readFile('./data_json/newGoods.json','utf8',(err,data)=>{
+        data = JSON.parse(data)
+        let saveCount = 0;
+        const Goods = mongoose.model('Goods');
         data.map((value,index)=>{
-            console.log(value)
-            let newGoods = new Goods(value)
+            console.log(value);
+            let newGoods = Goods(value);
             newGoods.save().then(()=>{
-                saveCount++
-                console.log('成功'+saveCount)
+                saveCount++;
+                console.log('成功'+saveCount);
             }).catch(error=>{
-                 console.log('失败：'+error)
+                console.log('失败'+saveCount);
             })
-        })
+        });
+        ctx.body='开始导入数据';
     })
-    ctx.body="开始导入数据"
+
 })
-module.exports=router;
+
+router.get('/insertAllCategoriesInfo',async(ctx)=>{
+
+    fs.readFile('./data_json/category.json','utf8',(err,data)=>{
+        data = JSON.parse(data)
+        let saveCount = 0;
+        const Category = mongoose.model('Category');
+        data.RECORDS.map((value,index)=>{
+            console.log(value);
+            let newCategory = Category(value);
+            newCategory.save().then(()=>{
+                saveCount++;
+                console.log('成功'+saveCount);
+            }).catch(error=>{
+                console.log('失败'+saveCount);
+            })
+        });
+        ctx.body='开始导入数据';
+    })
+
+})
+
+module.exports=router
---
+++ service/database/schema/Category.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+let ObjectId = Schema.Types.ObjectId
+
+const categorySchema = new Schema({
+    ID:{unique:true,type:String},
+    MALL_CATEGORY_NAME:{type:String},
+    IMAGE:{type:String},
+    TYPE:{type:Number},
+    SORT:{type:Number},
+    COMMENTS:{type:String}
+},{
+    collection:'categories'
+})
+
+mongoose.model('Category',categorySchema)
