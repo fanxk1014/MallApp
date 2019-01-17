b353840c3a2c083f7c64d19efe6da41661bd7369
xiaokun <fanxk1014@foxmail.com>
2019-01-17 17:33:19 +0800

    前端商品分类页；接口封装

--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -6,5 +6,8 @@ const URL = {
     registerUser: LOCALURL+'user/register',//用户注册接口
     loginUser: LOCALURL+'user/login',//用户注册接口
     getGoodsDetailInfo: LOCALURL+'goods/getGoodsDetailInfo',//获取商品详情接口
+    getCategoryList:LOCALURL+'goods/getCategoryList',         //得到大类信息
+    getCategorySubList:LOCALURL+'goods/getCategorySubList',   //得到小类信息
+    getGoodsBySubId:LOCALURL+'goods/getGoodsBySubId',   //得到小类商品信息
 }
 module.exports = URL
