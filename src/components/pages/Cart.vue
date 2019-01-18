624f92f86be9dffc5931650329a3be4208de0436
xiaokun <fanxk1014@foxmail.com>
2019-01-18 17:24:57 +0800

    新建购物车页面并添加路由

---
+++ src/components/pages/Cart.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="navbar-div">
+            <van-nav-bar title="购物车" /> 
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
